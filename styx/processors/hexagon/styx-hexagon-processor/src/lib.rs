f2d68af2418b5797e26710a5c7828f5fdd74e365
Ramesh Balaji <me@ramesh.tv>
2025-10-10 10:40:22 -0400

    feat: styx hexagon processor

--- styx/processors/hexagon/styx-hexagon-processor/src/lib.rs
+++ styx/processors/hexagon/styx-hexagon-processor/src/lib.rs
@@ -1,6 +1,16 @@
 // SPDX-License-Identifier: BSD-2-Clause
 //! # Styx-Processors
 
+use styx_core::cpu::arch::hexagon::HexagonVariants;
+use styx_core::{
+    core::{
+        builder::{BuildProcessorImplArgs, ProcessorImpl},
+        ProcessorBundle,
+    },
+    cpu::{ArchEndian, HexagonPcodeBackend},
+    errors::{anyhow, UnknownError},
+};
+
 #[derive(serde::Deserialize)]
 pub struct HexagonBuilder {
     pub variant: HexagonVariants,
@@ -9,9 +19,37 @@ pub struct HexagonBuilder {
 impl Default for HexagonBuilder {
     fn default() -> Self {
         Self {
-            variant: HexagonVariant::QDSP6V62,
+            variant: HexagonVariants::QDSP6V62,
         }
     }
 }
 
-impl ProcessorImpl for BlackfinBuilder {}
+impl ProcessorImpl for HexagonBuilder {
+    fn build(&self, args: &BuildProcessorImplArgs) -> Result<ProcessorBundle, UnknownError> {
+        let cpu = if let backend::pcode = args.backend {
+            Box::new(HexagonPcodeBackend::new_engine_config(
+                self.variant,
+                ArchEndian::LittleEndian,
+                &args.into(),
+            ))
+        } else {
+            return Err(anyhow::anyhow!(
+                "hexagon processor only supports pcode backend"
+            ));
+        };
+        let mut mmu = Mmu::default_region_store();
+        let cec = Box::new(CoreEventController::default());
+        let mut peripherals: Vec<Box<dyn Peripheral>> = Vec::new();
+
+        let mut hints = LoaderHints::new();
+        hints.insert("arch".to_string().into_boxed_str(), Box::new(Arch::Hexagon));
+
+        Ok(ProcessorBundle {
+            cpu: cpu,
+            mmu: mmu,
+            event_controller: cec,
+            peripherals,
+            loader_hints: hints,
+        })
+    }
+}
