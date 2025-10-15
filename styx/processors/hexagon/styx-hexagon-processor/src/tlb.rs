4e0fcce3f388b7d4197a7ce9aaf505d0b42cb350
Ramesh Balaji <me@ramesh.tv>
2025-10-14 20:30:03 -0400

    fix tlb algo

--- styx/processors/hexagon/styx-hexagon-processor/src/tlb.rs
+++ styx/processors/hexagon/styx-hexagon-processor/src/tlb.rs
@@ -19,6 +19,8 @@ struct Pte {
     ppd: u24,
     #[bits(24..=27, rw)]
     c: u4,
+    #[bit(27, rw)]
+    pte_hsv39: bool,
     #[bit(28, rw)]
     u: bool,
     #[bit(29, rw)]
@@ -49,6 +51,24 @@ pub struct HexagonTlb {
     enable_data_translation: bool,
 }
 
+const PAGE_SIZE_BITS: u64 = 12;
+// From https://github.com/quic/qemu/blob/3921c6eed6bd7c670eff633fe829e18607125969/hw/hexagon/hexagon_tlb.c
+const PAGE_MASK: [u64; 13] = [
+    0x0fff,
+    0x3fff,
+    0xffff,
+    0x3ffff,
+    0xfffff,
+    0x3fffff,
+    0xffffff,
+    0x3ffffff,
+    0xfffffff,
+    0x3fffffff,
+    0xffffffff,
+    0x3ffffffff,
+    0xfffffffff,
+];
+
 impl HexagonTlb {
     pub fn new() -> Self {
         Self {
@@ -56,6 +76,16 @@ impl HexagonTlb {
             enable_data_translation: false,
             entries: [Pte::new_with_raw_value(0); MAX_TLB_ENTRIES],
         }
+    }
+
+    // https://github.com/quic/qemu/blob/3921c6eed6bd7c670eff633fe829e18607125969/hw/hexagon/hexagon_tlb.c#L100
+    fn get_entry_page_type(ent: &Pte) -> usize {
+        ent.ppd().trailing_zeros() as usize // + (ent.pte_hsv39() as usize * 4)
+    }
+
+    // https://github.com/quic/qemu/blob/3921c6eed6bd7c670eff633fe829e18607125969/hw/hexagon/hexagon_tlb.c#L100
+    fn get_entry_page_num_bits(ent: &Pte) -> u64 {
+        PAGE_SIZE_BITS + (2 * Self::get_entry_page_type(ent) as u64)
     }
 }
 
@@ -91,34 +121,37 @@ impl TlbImpl for HexagonTlb {
             Ok(virt_addr)
         } else {
             let virt_addr = virt_addr as u32;
-            // 4k page
-            let vpn = virt_addr >> 16;
-            let offset = virt_addr & 0xffff;
 
             for ent in &self.entries {
                 if !ent.v() {
                     continue;
                 }
 
-                let ppd = u64::from((ent.ppd() >> 5)).overflowing_shl(16).0;
-                let ent_vpn = u32::from(ent.vpn()) >> 4;
+                // It appears that tlb entries have varying page sizes that are encoded
 
-                info!("ent vpn {:x} real vpn {:x}", ent_vpn, vpn);
-                info!(
-                    "ent ppd {:x} shift ppd {:x} off {:x}",
-                    ent.ppd(),
-                    ppd,
-                    offset
-                );
-                if ent_vpn == vpn {
-                    let pa = u64::from(ppd + offset as u64);
-                    info!("translated {virt_addr:x} to {pa:x}");
-                    return Ok(pa as u64);
+                let page_type = Self::get_entry_page_type(ent);
+                let vpn_shift = PAGE_SIZE_BITS;
+
+                let va_vpn = (virt_addr as u64) & !PAGE_MASK[page_type];
+                let va_offset = virt_addr as u64 & PAGE_MASK[page_type];
+
+                let ent_vpn = u64::from(ent.vpn()) << PAGE_SIZE_BITS;
+
+                trace!("ent vpn {ent_vpn:x} real vpn {va_vpn:x} va_offset {va_offset:x}",);
+                if va_vpn == ent_vpn {
+                    let ppd_mask = u64::from(ent.ppd() >> 1)
+                        .overflowing_shl(PAGE_SIZE_BITS as u32)
+                        .0
+                        & !PAGE_MASK[page_type];
+                    let pa = ppd_mask + va_offset;
+
+                    trace!("va {virt_addr:x} ppd_mask {ppd_mask:x} pa {pa:x}");
+                    return Ok(pa);
                 }
             }
 
             error!("couldn't translate {virt_addr:x}");
-            // lol what
+            //
             Err(TlbTranslateError::Other(UnknownError::msg("tlb failed")))
         }
     }
@@ -127,9 +160,10 @@ impl TlbImpl for HexagonTlb {
         let pte = Pte::new_with_raw_value(data);
         self.entries[idx] = pte;
         info!(
-            "tlb inserted at {idx} was {pte:x?} PA 0x{:x} VA 0x{:x}",
+            "tlb {data:x} inserted at {idx} was {pte:x?} PA 0x{:x} VA 0x{:x} page size bits {:?}",
             (pte.ppd() >> 1).overflowing_shl(12).0,
-            pte.vpn().overflowing_shl(12).0
+            pte.vpn().overflowing_shl(12).0,
+            Self::get_entry_page_num_bits(&pte)
         );
         Ok(())
     }
