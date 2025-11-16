9e99180ece318c5e07428925bc32286717cd5501
Ramesh Balaji <me@ramesh.tv>
2025-11-16 12:13:50 -0500

    fix: rewrite testbit

--- styx/core/styx-cpu-pcode-backend/src/arch_spec/hexagon/tests/arith.rs
+++ styx/core/styx-cpu-pcode-backend/src/arch_spec/hexagon/tests/arith.rs
@@ -65,7 +65,7 @@ pub fn testbit_reg() {
     let exit = cpu.execute(&mut mmu, &mut ev, 1).unwrap();
     assert_eq!(exit.exit_reason, TargetExitReason::InstructionCountComplete);
 
-    let p0 = cpu.read_register::<u8>(HexagonRegister::P0).unwrap() as i32;
+    let p0 = cpu.read_register::<u8>(HexagonRegister::P0).unwrap();
     assert_eq!(p0, 0xff);
 }
 
@@ -83,6 +83,23 @@ pub fn testbit_reg_f() {
     let exit = cpu.execute(&mut mmu, &mut ev, 1).unwrap();
     assert_eq!(exit.exit_reason, TargetExitReason::InstructionCountComplete);
 
-    let p0 = cpu.read_register::<u8>(HexagonRegister::P0).unwrap() as i32;
+    let p0 = cpu.read_register::<u8>(HexagonRegister::P0).unwrap();
+    assert_eq!(p0, 0x00);
+}
+
+#[test]
+pub fn testbit_reg_oob() {
+    let (mut cpu, mut mmu, mut ev) = setup_objdump(
+        r#"
+       0:	00 c0 02 c7	c702c000 { 	p0 = tstbit(r2,r0) }
+"#,
+    );
+
+    cpu.write_register(HexagonRegister::R2, 0u32).unwrap();
+    cpu.write_register(HexagonRegister::R0, 0x80du32).unwrap();
+    let exit = cpu.execute(&mut mmu, &mut ev, 1).unwrap();
+    assert_eq!(exit.exit_reason, TargetExitReason::InstructionCountComplete);
+
+    let p0 = cpu.read_register::<u8>(HexagonRegister::P0).unwrap();
     assert_eq!(p0, 0x00);
 }
