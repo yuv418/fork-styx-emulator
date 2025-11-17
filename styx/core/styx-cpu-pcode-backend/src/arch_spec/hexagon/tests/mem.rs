d2fcf53397efcb981434b7834d019f769c8081b9
Ramesh Balaji <me@ramesh.tv>
2025-11-16 19:20:29 -0500

    fix: store instructions not working properly

--- styx/core/styx-cpu-pcode-backend/src/arch_spec/hexagon/tests/mem.rs
+++ styx/core/styx-cpu-pcode-backend/src/arch_spec/hexagon/tests/mem.rs
@@ -21,3 +21,33 @@ fn test_mem_signextend_byte() {
     let r0_read = cpu.read_register::<u32>(HexagonRegister::R0).unwrap();
     assert_eq!(r0_read, 0xffffffff);
 }
+
+#[test]
+fn test_mem_load_halfword() {
+    let (mut cpu, mut mmu, mut ev) = setup_objdump(
+        r#"
+       0:	06 40 82 91	91824006 { 	r6 = memw(r2+#0x0)
+       4:	81 c0 24 3c	3c24c081   	memh(r4+#0x2) = #0x1 } 
+"#,
+    );
+
+    const ADDR2: u32 = 0x10000;
+    const ADDR4: u32 = 0x20000;
+
+    mmu.write_u32_le_virt_data(ADDR2 as u64, 0x99887766, &mut cpu)
+        .unwrap();
+    mmu.write_u32_le_virt_data(ADDR4 as u64, 0xbfbfbfbf, &mut cpu)
+        .unwrap();
+
+    cpu.write_register(HexagonRegister::R2, ADDR2).unwrap();
+    cpu.write_register(HexagonRegister::R4, ADDR4 - 2).unwrap();
+
+    let exit = cpu.execute(&mut mmu, &mut ev, 1).unwrap();
+    assert_eq!(exit.exit_reason, TargetExitReason::InstructionCountComplete);
+
+    let dat_read = mmu.read_u32_le_virt_data(ADDR4 as u64, &mut cpu).unwrap();
+    let r6_read = cpu.read_register::<u32>(HexagonRegister::R6).unwrap();
+
+    assert_eq!(dat_read, 0xbfbf0001);
+    assert_eq!(r6_read, 0x99887766);
+}
