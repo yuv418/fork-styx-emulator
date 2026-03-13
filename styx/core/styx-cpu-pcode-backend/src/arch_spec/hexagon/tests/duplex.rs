767bc7b7951f53ff9e9ea14ea36194daebc9d605
Ramesh Balaji <me@ramesh.tv>
2026-03-12 23:03:59 -0700

    fix: addi and addrx for Rx16

--- styx/core/styx-cpu-pcode-backend/src/arch_spec/hexagon/tests/duplex.rs
+++ styx/core/styx-cpu-pcode-backend/src/arch_spec/hexagon/tests/duplex.rs
@@ -97,3 +97,28 @@ fn broken_duplex_add() {
     assert_eq!(r0, MAGIC);
     assert_eq!(r16, R16ADDR + 8);
 }
+
+#[test]
+fn broken_duplex_reg_add() {
+    let (mut cpu, mut mmu, mut ev) = setup_objdump(
+        r#"
+0:	80 00 98 58	58980080 { 	r16 = add(r16,r17); 	r0 = memw(r16+#0x0) }
+"#,
+    );
+
+    const R16ADDR: u32 = 0x100u32;
+    const MAGIC: u32 = 0xaa887652u32;
+
+    cpu.write_register(HexagonRegister::R16, R16ADDR).unwrap();
+    cpu.write_register(HexagonRegister::R17, 8u32).unwrap();
+    mmu.write_u32_le_phys_data(R16ADDR as u64, MAGIC).unwrap();
+
+    let exit = cpu.execute(&mut mmu, &mut ev, 1).unwrap();
+    assert_eq!(exit.exit_reason, TargetExitReason::InstructionCountComplete);
+
+    let r0 = cpu.read_register::<u32>(HexagonRegister::R0).unwrap();
+    let r16 = cpu.read_register::<u32>(HexagonRegister::R16).unwrap();
+
+    assert_eq!(r0, MAGIC);
+    assert_eq!(r16, R16ADDR + 8);
+}
